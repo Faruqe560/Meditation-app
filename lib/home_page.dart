fb8e65b792a8a8df986387493febd1fac96d1504
Faruqe560 <faruqehassan7@gmail.com>
2022-07-03 14:53:08 +0600

    home page

---
+++ lib/home_page.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Color.fromARGB(188, 248, 242, 242),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                height: screenHeight * 0.43,
+                width: screenWidth,
+                color: Color(0xfff5ceb8),
+                child: Container(
+                  margin: EdgeInsets.only(right: 40, top: 20, bottom: 20),
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/path.png"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              SizedBox(
+                height: 60,
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Good Morning \nFaruqe",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              Container(
+                height: screenHeight * 0.07,
+                width: screenWidth * 0.9,
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(48),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            labelText: "Search",
+                            labelStyle: TextStyle(fontSize: 16)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  childAspectRatio: .85,
+                  children: [
+                    categoryWidget("img1", "Diet Recommendation"),
+                    categoryWidget("img2", "Kegel Exercise"),
+                    categoryWidget("img3", "Meditation"),
+                    categoryWidget("img4", "Yoga"),
+                    categoryWidget("img1", "Diet Recommendation"),
+                    categoryWidget("img2", "Kegel Exercise"),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        FaIcon(FontAwesomeIcons.calendar),
+                        Text("Today")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        FaIcon(FontAwesomeIcons.dumbbell),
+                        Text("All Exercise")
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container categoryWidget(String img, String title) {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10, bottom: 20),
+      width: MediaQuery.of(context).size.width * 0.3,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 20,
+            spreadRadius: 1,
+            offset: Offset(0, 1),
+          ),
+        ],
+      ),
+      child: InkWell(
+        onTap: () {
+          openProductPage('$img', '$title');
+        },
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/$img.png"),
+                      fit: BoxFit.contain),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "$title",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+            ),
+            SizedBox(
+              height: 7,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void openProductPage(String img, String title) {
+    Navigator.pushNamed(context, "/productPage",
+        arguments: {'image': '$img', 'title': '$title'});
+  }
+}
